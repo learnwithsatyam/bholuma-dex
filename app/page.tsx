f1f9a27cc3db983a30ff32e718c1afe3c8805721
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-02 05:34:05 +0530

    added the support for custom coordinates for floating image card and rotate animation for the card

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-zinc-900 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" />
+    <div className=" relative bg-zinc-900 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={50} left={200} />
+      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={400} left={600} />
+      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={500} left={400} />
+      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={300} left={400} />
     </div>
   );
 }
--- bholuma-components/floating-image-card.tsx
+++ bholuma-components/floating-image-card.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import Image from 'next/image';
 
-function FloatingImageCard({imageUrl, alt}: {imageUrl: string, alt: string}) {
+function FloatingImageCard({imageUrl, alt, top, left}: {imageUrl: string, alt: string, top: number, left: number}) {
   if (!imageUrl) {
     return null;
   }
   return (
-    <div className="absolute top-50 float-animate w-[100px] h-[100px] rounded-full">
+    <div className="absolute float-animate w-[100px] h-[100px] rounded-full"
+    style={{ top: `${top}px`, left: `${left}px` }}>
         {/* Blurred halo */}
         <Image
           src={imageUrl}
           fill
           alt={alt}
-          className="absolute blur-sm opacity-40 rounded-full z-0"
+          className="absolute blur-sm opacity-40 rounded-full z-0 rotate-left-right"
         />
         {/* Actual sharp icon */}
         <Image
           src={imageUrl}
           fill
           alt={alt}
-          className="opacity-0 hover:scale-110 hover:opacity-100 -rotate-10 hover:rotate-0 transition-all duration-300 ease-in-out"
+          className="rotate-left-right opacity-0 hover:scale-110 hover:opacity-100 -rotate-10 hover:rotate-0 transition-all duration-300 ease-in-out"
         />
       </div>
 
