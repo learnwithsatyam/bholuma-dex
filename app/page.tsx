a87b245d1243f4b4dc10f02e92667acf7bf68ff2
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-03 02:31:57 +0530

    added the token images from github registry. Need to download all images and then use them locally or figure out how to fetch latest ones and temporarily adding a url in next config to allow images from that source

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 import FloatingImageCard from "@/bholuma-components/floating-image-card";
+import Navbar from "@/bholuma-components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" relative bg-zinc-900 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={50} left={50} animationDuration={4} />
-      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={40} left={60} animationDuration={2}/>
-      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={50} left={40} animationDuration={6}/>
-      <FloatingImageCard imageUrl="/metamaskDisk.png" alt="Metamask Disk" top={30} left={40} animationDuration={8}/>
+    <div className=" relative bg-zinc-900 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+
+      // TRUMP
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/trUmpZnz3uf4pHDwsVbdpEeMygiiVCpJq9ZgoWcaWBz/logo.png" alt="Metamask Disk" blur={5} width={100} top={50} left={50} animationDuration={4} />
+
+      // SERUM
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt/logo.png" alt="Metamask Disk" blur={10} width={50} top={40} left={60} animationDuration={2}/>
+
+      // MANGO
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac/token.png" alt="Metamask Disk" blur={20} width={150} top={50} left={40} animationDuration={6}/>
+
+      // USDC
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png" alt="Metamask Disk" blur={30} width={90} top={30} left={40} animationDuration={8}/> 
+      
+      // JUPITER
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/SergeyDobrinyn/token/master/files/jupiter.png" alt="Metamask Disk" blur={2} width={30} top={10} left={20} animationDuration={8}/> 
+      
+      // RAYDIUM
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R/logo.png" alt="Metamask Disk" blur={7} width={100} top={70} left={80} animationDuration={8}/> 
+      
+      // BONK
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/6dhTynDkYsVM7cbF7TKfC9DWB636TcEM935fq7JzL2ES/logo.png" alt="Metamask Disk" blur={20} width={100} top={10} left={90} animationDuration={8}/> 
+      
+      // ORCA
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/orcaEKTdK7LKz57vaAYr9QeNsVEPfiu6QeMU1kektZE/logo.png" alt="Metamask Disk" blur={20} width={100} top={90} left={10} animationDuration={8}/> 
+      
+      // USDT
+      <FloatingImageCard imageUrl="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/D3KdBta3p53RV5FoahnJM5tP45h6Fd3AyFYgXTJvGCaK/logo.svg" alt="Metamask Disk" blur={20} width={100} top={90} left={40} animationDuration={8}/> 
+      
+      
     </div>
   );
 }
