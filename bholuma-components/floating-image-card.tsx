7fb00fd2cef4daade255bfbed969e191283d0b4e
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-03 02:30:18 +0530

    made the floating image card component proper for real use

--- bholuma-components/floating-image-card.tsx
+++ bholuma-components/floating-image-card.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import Image from 'next/image';
 
-function FloatingImageCard({imageUrl, alt, top, left, animationDuration}: {imageUrl: string, alt: string, top: number, left: number, animationDuration: number}) {
+function FloatingImageCard({imageUrl, alt, top, left, animationDuration, width, blur}: {imageUrl: string, alt: string, top: number, left: number, animationDuration: number, width: number, blur?: number}) {
   if (!imageUrl) {
     return null;
   }
   return (
-    <div className="absolute float-animate w-[100px] h-[100px] rounded-full"
-    style={{ top: `${top}%`, left: `${left}%`, animationDelay: `${Math.random() * 5}s`, animationDuration: `${animationDuration}s` }}>
+    <div className="absolute float-animate rounded-full"
+    style={{ width: `${width}px`, height: `${width}px`, top: `${top}%`, left: `${left}%`, animationDelay: `${Math.random() * 5}s`, animationDuration: `${animationDuration}s` }}>
         {/* Blurred halo */}
         <Image
           src={imageUrl}
           fill
           alt={alt}
-          className="absolute blur-sm opacity-40 rounded-full z-0 rotate-left-right"
+          className="absolute opacity-90 rounded-full z-0 rotate-left-right"
+          style={{ filter: `blur(${blur ?? 20}px)`}}
         />
         {/* Actual sharp icon */}
         <Image
           src={imageUrl}
           fill
           alt={alt}
-          className="rotate-left-right opacity-0 hover:scale-110 hover:opacity-100 -rotate-10 hover:rotate-0 transition-all duration-300 ease-in-out"
+          className="rotate-left-right rounded-full opacity-0 hover:scale-110 hover:opacity-100 -rotate-10 hover:rotate-0 transition-all duration-300 ease-in-out"
         />
       </div>
 
