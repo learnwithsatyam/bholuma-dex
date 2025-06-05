1555256e0edbd103b2554af78478afe860d09a10
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-06 04:21:37 +0530

    fixing the handleTokenSwap and other things in dex component

--- bholuma-components/dex.tsx
+++ bholuma-components/dex.tsx
@@ -16,10 +16,11 @@ import getTokenPrice from '@/bl/getTokenPrice'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import ConnectWallet from '@/bholuma-components/WalletConnectButton'
 import { JupiterSwapTransactionRequestInterface } from '@/interfaces/jupiterSwapTransactionRequestInterface'
-import { LAMPORTS_PER_SOL, PublicKey, Transaction } from '@solana/web3.js'
+import { LAMPORTS_PER_SOL, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js'
 import { createSwapTransaction } from '@/bl/swapTokens'
 import { jupiterSwapTransactionResponseInterface } from '@/interfaces/jupiterSwapTransactionResponseInterface'
-import { connect } from 'http2'
+import { toast } from 'sonner'
+import { BholumaButton } from './BholumaButton'
 
 function Dex({ tokens }: { tokens: SolanaTokenInterface[] }) {
 
@@ -30,8 +31,10 @@ function Dex({ tokens }: { tokens: SolanaTokenInterface[] }) {
     const buyTokenAddress = useSelector((state: RootState) => state.buyAndSellToken.buyTokenAddress);
     const buyTokenAmount = useSelector((state: RootState) => state.buyAndSellToken.buyTokenAmount);
 
+    const dispatch = useDispatch();
+
     const { publicKey, disconnect, wallet } = useWallet();
-    const {connection} = useConnection();
+    const { connection } = useConnection();
 
     const sellToken = SUPPORTED_TOKENS.find((token) => token.address == sellTokenAddress);
     const buyToken = SUPPORTED_TOKENS.find((token) => token.address == buyTokenAddress);
@@ -78,107 +81,127 @@ function Dex({ tokens }: { tokens: SolanaTokenInterface[] }) {
 
 
     const handleTokenSwap = async () => {
-        console.log("currentQuote: ", currentQuote);
-        const requestBody: JupiterSwapTransactionRequestInterface = {
-            userPublicKey: publicKey?.toBase58() as string,
-            quoteResponse: currentQuote!,
-            prioritizationFeeLamports: {
-                priorityLevelWithMaxLamports: {
-                    maxLamports: 0.001 * LAMPORTS_PER_SOL,
-                    priorityLevel: "high"
-                },
-                jitoTipLamports: 0
-            },
-            dynamicComputeUnitLimit: true,
-            feeAccount: sellTokenAddress,
-            blockhashSlotsToExpiry: 20
-        }
-
-        const swapTransaction: jupiterSwapTransactionResponseInterface | null = await createSwapTransaction(requestBody);
-
-        if (!swapTransaction) {
+        if(!sellTokenAddress || !buyTokenAddress) {
+            toast.error("Please select the sell and buy tokens first.");
             return;
         }
 
-        const transaction = Transaction.from(
-            Buffer.from(swapTransaction!.swapTransaction!, "base64"));
-
-        const signature = await wallet?.adapter.sendTransaction(transaction, connection);
-        if (!signature) {
-            throw new Error("Failed to get transaction signature.");
+        if(sellTokenAmount<=0){
+            toast.error("Please enter the amount to sell.")
+            return;
         }
-        const latestBlock = await connection.getLatestBlockhash();
-        const result = await connection.confirmTransaction({
-            lastValidBlockHeight: latestBlock.lastValidBlockHeight,
-            blockhash: latestBlock.blockhash,
-            signature: signature
-        }, "finalized");
+        try {
+            const requestBody: JupiterSwapTransactionRequestInterface = {
+                userPublicKey: publicKey?.toBase58() as string,
+                quoteResponse: currentQuote!,
+                prioritizationFeeLamports: {
+                    priorityLevelWithMaxLamports: {
+                        maxLamports: 0.001 * LAMPORTS_PER_SOL,
+                        priorityLevel: "high"
+                    },
+                },
+                dynamicComputeUnitLimit: true,
+                feeAccount: sellTokenAddress,
+            }
 
+            const swapTransaction: jupiterSwapTransactionResponseInterface | string = await createSwapTransaction(requestBody);
+
+            if (typeof swapTransaction === "string") {
+                toast.error(swapTransaction);
+                return;
+            }
+
+            const transaction = VersionedTransaction.deserialize(
+                Buffer.from(swapTransaction!.swapTransaction!, "base64")
+            );
+
+            const signature = await wallet?.adapter.sendTransaction(transaction, connection);
+            if (!signature) {
+                throw new Error("Failed to get transaction signature.");
+            }
+            const latestBlock = await connection.getLatestBlockhash();
+            const result = await connection.confirmTransaction({
+                lastValidBlockHeight: latestBlock.lastValidBlockHeight,
+                blockhash: latestBlock.blockhash,
+                signature: signature
+            }, "finalized");
+
+            if(result.value.err){
+                toast.error(`Something went wrong: ${result.value.err}`)
+                return;
+            }
+
+            toast(`Tokens swapped successfully. ${result.context.slot.toString()}`)
+        } catch (err: any) {
+            if (err?.message?.includes("User rejected the request")) {
+                toast.info("You cancelled the transaction.");
+            } else {
+                console.error("Transaction error:", err);
+                toast.error("Transaction failed. Please try again.");
+            }
+        }
+    }
+        useEffect(() => {
+            getCurrentQuote(); // Clean up on unmount
+        }, [sellTokenAmount, sellTokenAddress, buyTokenAddress]);
+
+        return (
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <h1 className="text-4xl font-bold text-center text-white mb-4">Welcome to <br /> Bholuma Dex</h1>
+                <div className="text-lg text-gray-300 bg-zinc-950 space-y-2 p-2 rounded-xl shadow-lg">
+                    <div className='grid grid-rows-[auto_1fr_auto] items-center justify-items-center bg-black rounded-md px-2 py-2 space-y-4 min-h-30 min-w-120'>
+                        <Label className="text-slate-500 text-left text-sm w-full">
+                            SELL
+                        </Label>
+                        <div className='flex items-center rounded-md w-full'>
+                            <Input type="text" placeholder="0" className='[&&]:text-4xl px-0 [&&]:h-fit [&&]:!border-none [&&]:!outline-none [&&]:!ring-0 [&&]:!focus-visible:ring-0 [&&]:!focus-visible:border-none [&&]:!focus-visible:outline-none'
+                                inputMode="numeric"
+                                pattern="[0-9]*"
+                                onChange={(e) => {
+                                    const val = e.target.value;
+                                    if (/^\d*\.?\d*$/.test(val) || val == '') {
+                                        dispatch(setSellTokenAmount(Number(val))); // only set state if it's an integer or empty
+                                    } else {
+                                        e.target.value = ''; // reset input if invalid
+                                    }
+                                }} />
+                            <TokenSelector tokens={tokens ?? []} address={sellTokenAddress} setAddress={changeSellTokenAddress} />
+                        </div>
+                        <Label className="text-slate-500 text-left text-sm w-full">
+                            ${sellTokenPrice}
+                        </Label>
+                    </div>
+                    <div className='grid grid-rows-[auto_1fr_auto] items-center justify-items-center px-2 py-2 bg-gray-800 rounded-md space-y-4 min-h-30 min-w-120'>
+                        <Label className="text-slate-500 text-left text-sm w-full">
+                            BUY
+                        </Label>
+                        <div className='flex items-center rounded-md w-full'>
+                            <Input type="text" disabled placeholder="0" className='[&&]:text-4xl px-0 [&&]:h-fit [&&]:!border-none [&&]:!outline-none [&&]:!ring-0 [&&]:!focus-visible:ring-0 [&&]:!focus-visible:border-none [&&]:!focus-visible:outline-none'
+                                inputMode="numeric"
+                                pattern="[0-9]*"
+                                value={buyTokenAmount}
+                            />
+                            <TokenSelector tokens={tokens ?? []} address={buyTokenAddress} setAddress={changeBuyTokenAddress} />
+                        </div>
+                        <Label className="text-slate-500 text-left text-sm w-full">
+                            ${buyTokenPrice}
+                        </Label>
+                    </div>
+                    {publicKey ?
+                        <div className='flex items-center w-full space-x-2 px-2 justify-center'>
+                            <BholumaButton className="bg-orange-600 hover:bg-orange-700 text-white w-1/2 font-bold py-2 px-4 rounded-md p-6" onClick={handleTokenSwap}>
+                                Swap
+                            </BholumaButton>
+                            <Button className="bg-gray-600 hover:bg-gray-700 text-white w-1/2 font-bold py-2 px-4 rounded-md p-6" onClick={disconnect}>
+                                Disconnect
+                            </Button>
+                        </div>
+                        :
+                        <ConnectWallet />
+                    }
+                </div>
+            </div>
+        )
     }
 
-    useEffect(() => {
-        getCurrentQuote(); // Clean up on unmount
-    }, [sellTokenAmount, sellTokenAddress, buyTokenAddress]);
-
-    const dispatch = useDispatch();
-
-    return (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <h1 className="text-4xl font-bold text-center text-white mb-4">Welcome to <br /> Bholuma Dex</h1>
-            <div className="text-lg text-gray-300 bg-zinc-950 space-y-2 p-2 rounded-xl shadow-lg">
-                <div className='grid grid-rows-[auto_1fr_auto] items-center justify-items-center bg-black rounded-md px-2 py-2 space-y-4 min-h-30 min-w-120'>
-                    <Label className="text-slate-500 text-left text-sm w-full">
-                        SELL
-                    </Label>
-                    <div className='flex items-center rounded-md w-full'>
-                        <Input type="text" placeholder="0" className='[&&]:text-4xl px-0 [&&]:h-fit [&&]:!border-none [&&]:!outline-none [&&]:!ring-0 [&&]:!focus-visible:ring-0 [&&]:!focus-visible:border-none [&&]:!focus-visible:outline-none'
-                            inputMode="numeric"
-                            pattern="[0-9]*"
-                            onChange={(e) => {
-                                const val = e.target.value;
-                                if (/^\d*\.?\d*$/.test(val) || val == '') {
-                                    dispatch(setSellTokenAmount(Number(val))); // only set state if it's an integer or empty
-                                } else {
-                                    e.target.value = ''; // reset input if invalid
-                                }
-                            }} />
-                        <TokenSelector tokens={tokens ?? []} address={sellTokenAddress} setAddress={changeSellTokenAddress} />
-                    </div>
-                    <Label className="text-slate-500 text-left text-sm w-full">
-                        ${sellTokenPrice}
-                    </Label>
-                </div>
-                <div className='grid grid-rows-[auto_1fr_auto] items-center justify-items-center px-2 py-2 bg-gray-800 rounded-md space-y-4 min-h-30 min-w-120'>
-                    <Label className="text-slate-500 text-left text-sm w-full">
-                        BUY
-                    </Label>
-                    <div className='flex items-center rounded-md w-full'>
-                        <Input type="text" disabled placeholder="0" className='[&&]:text-4xl px-0 [&&]:h-fit [&&]:!border-none [&&]:!outline-none [&&]:!ring-0 [&&]:!focus-visible:ring-0 [&&]:!focus-visible:border-none [&&]:!focus-visible:outline-none'
-                            inputMode="numeric"
-                            pattern="[0-9]*"
-                            value={buyTokenAmount}
-                        />
-                        <TokenSelector tokens={tokens ?? []} address={buyTokenAddress} setAddress={changeBuyTokenAddress} />
-                    </div>
-                    <Label className="text-slate-500 text-left text-sm w-full">
-                        ${buyTokenPrice}
-                    </Label>
-                </div>
-                {publicKey ?
-                    <div className='flex items-center w-full space-x-2 px-2 justify-center'>
-                        <Button className="bg-pink-600 hover:bg-pink-700 text-white w-1/2 font-bold py-2 px-4 rounded-md p-6" onClick={handleTokenSwap}>
-                            Swap
-                        </Button>
-                        <Button className="bg-gray-600 hover:bg-gray-700 text-white w-1/2 font-bold py-2 px-4 rounded-md p-6" onClick={disconnect}>
-                            Disconnect
-                        </Button>
-                    </div>
-                    :
-                    <ConnectWallet />
-                }
-            </div>
-        </div>
-    )
-}
-
-export default Dex
+    export default Dex
