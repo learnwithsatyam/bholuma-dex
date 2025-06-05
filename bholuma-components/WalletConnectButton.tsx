5d69aba02198ccc5e43fb280334d74746102743f
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-05 07:43:35 +0530

    Wallet connect button was rendering again and again with hydration error. This is fixed now.

--- bholuma-components/WalletConnectButton.tsx
+++ bholuma-components/WalletConnectButton.tsx
@@ -1,5 +1,15 @@
+"use client";
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useEffect, useState } from 'react';
 
 export default function ConnectWallet() {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null; // ⛔️ Prevent hydration mismatch
+
   return <WalletMultiButton className="bg-pink-600 hover:bg-pink-900 text-white w-full font-bold py-2 px-4 rounded-md p-6" />
 }
