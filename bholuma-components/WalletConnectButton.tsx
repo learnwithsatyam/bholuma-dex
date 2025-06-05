bff573a16e1b68e7a5cf8e01d119d037c0686096
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-06 04:17:59 +0530

    made custom button in WalletConnectButton

--- bholuma-components/WalletConnectButton.tsx
+++ bholuma-components/WalletConnectButton.tsx
@@ -1,15 +1,22 @@
 "use client";
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useWalletModal, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import { useEffect, useState } from 'react';
 
 export default function ConnectWallet() {
   const [hasMounted, setHasMounted] = useState(false);
-
+  const { setVisible } = useWalletModal();
   useEffect(() => {
     setHasMounted(true);
   }, []);
 
   if (!hasMounted) return null; // ⛔️ Prevent hydration mismatch
 
-  return <WalletMultiButton className="bg-pink-600 hover:bg-pink-900 text-white w-full font-bold py-2 px-4 rounded-md p-6" />
+  return (
+    <button
+      onClick={() => setVisible(true)}
+      className="bg-orange-600 hover:bg-orange-900  transition delay-150 duration-300 ease-in-out text-white w-full font-bold py-2 px-4 rounded-md"
+    >
+      Connect Wallet
+    </button>
+  );
 }
