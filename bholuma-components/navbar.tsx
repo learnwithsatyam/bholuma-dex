9f10f795e2087902c7f858047e8bab696ffee4d0
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-09 19:29:11 +0530

    making the navbar responsive

--- bholuma-components/navbar.tsx
+++ bholuma-components/navbar.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { FaGithub, FaSquareXTwitter, FaLinkedin } from "react-icons/fa6";
 import { useEffect, useState } from "react";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
 export const Navbar = () => {
     const [hasMounted, setHasMounted] = useState(false);
     useEffect(() => {
@@ -15,7 +18,7 @@ export const Navbar = () => {
 
     return (
         <nav className="sticky top-0 z-50 w-full bg-gradient-to-b from-black/50 to-transparent backdrop-blur-md">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between hidden md:flex">
                 {/* Logo */}
                 <Link href="/" className="flex items-center space-x-2">
                     <Image src="/bholumaIcon.png" alt="Bholuma" width={32} height={32} />
@@ -49,6 +52,38 @@ export const Navbar = () => {
 
                 {/* Wallet Button */}
                 <WalletMultiButton className="!bg-pink-600 !hover:bg-pink-700 !text-white !font-semibold !rounded-md !px-4 !py-2" />
+            </div>
+            <div className="md:hidden flex items-center mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between ">
+                <div className="w-1/2 flex items-center border-red-3 justify-start">
+                    {/* Logo */}
+                    <Link href="/" className="flex items-center space-x-2">
+                        <Image src="/bholumaIcon.png" alt="Bholuma" width={32} height={32} />
+                        <span className="text-xl font-bold text-white">Bholuma DEX</span>
+                    </Link>
+                </div>
+                <Drawer>
+                    <DrawerTrigger className="w-1/2 flex items-center justify-end text-black dark:text-white"><MenuIcon className="text-white" /></DrawerTrigger>
+                    <DrawerContent className="dark">
+                        <DrawerHeader>
+                            <DrawerTitle className="text-lg font-semibold">Menu</DrawerTitle>
+                        </DrawerHeader>
+                        <div className="flex flex-col gap-4 px-4 py-2">
+                            <Link href="https://x.com/sattyshivhare" target="_blank" className="text-sm text-muted-foreground hover:text-orange-500 flex justify-center items-center gap-2">
+                                <FaSquareXTwitter /> Twitter
+                            </Link>
+                            <Link href="https://github.com/learnwithsatyam" target="_blank" className="text-sm text-muted-foreground hover:text-orange-500 flex justify-center items-center gap-2">
+                                <FaGithub /> GitHub
+                            </Link>
+                            <Link href="https://linkedin.com/in/satyam-shivhare" target="_blank" className="text-sm text-muted-foreground hover:text-orange-500 flex justify-center items-center gap-2">
+                                <FaLinkedin /> LinkedIn
+                            </Link>
+                            <div className="flex items-center justify-center">
+                            <WalletMultiButton className="!bg-pink-600 !hover:bg-pink-700 !text-white !font-semibold !rounded-md !px-4 !py-2 w-fit" />
+                            </div>
+                        </div>
+
+                    </DrawerContent>
+                </Drawer>
             </div>
         </nav>
     );
